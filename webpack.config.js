13c823f7b6d94c8c32617db93290dd30325d3287
wht <wht1835@163.com>
2019-10-08 14:29:34 +0800

    webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const path = require('path')
 const UglifyPlugin = require('uglifyjs-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')   // 将样式文件分离
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   // 入口文件
@@ -14,7 +15,7 @@ module.exports = {
   // 输出（路径及文件夹）
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
 
   // loader 一种处理多种文件格式的机制，负责吧某种文件转换成webpack可以支持打包的模块
@@ -27,7 +28,7 @@ module.exports = {
         ],
         use: 'babel-loader',  // 指定使用的loader
       },
-      {
+/*       {
         test: /\.css/,   // 处理样式文件：使用css-loader，style-loader转换
         include: [
           path.resolve(__dirname, 'src'),
@@ -36,8 +37,17 @@ module.exports = {
           'css-loader',
           'style-loader',
         ]
-      },
+      }, */
       {
+        test: /\.less$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+            MiniCSSExtractPlugin.loader,
+            'css-loader',
+            'less-loader'
+        ]
+    },
+/*       {
         test: /\.less/,   // 处理样式预编译文件：使用css-loader，style-loader转换
 
         use: ExtractTextPlugin.extract({
@@ -48,7 +58,7 @@ module.exports = {
           ]
         })
         
-      },
+      }, */
       {
         test: /\.(jpg|png|gif)$/,   // 处理图片文件
         use: [
@@ -61,15 +71,23 @@ module.exports = {
     ]
 
   
+  }, 
+
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, 'src')
+    ],
+    extensions: [".wasm", ".mjs", ".js", ".json", ".jsx"]
   },
 
   // 插件 
   plugins: [
-    new ExtractTextPlugin('index.css'),  // 分离样式文件的插件
+    new MiniCSSExtractPlugin('[name].css'),  // 分离样式文件的插件
     new UglifyPlugin(),   // 压缩js代码的插件
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: 'assets/index.html'
+      template: './src/index.html'
     }),   // 关联html文件
   ]
   
